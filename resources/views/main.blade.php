5fbf43cf337964db5b104e747d31ee3856183f02
n3shie1k <black2003@inbox.lv>
2024-06-15 19:07:43 +0300

    main view

---
+++ resources/views/main.blade.php
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyGameList</title>
+    
+</head>
+<body>
+    <div class="container">
+		<h1>Welcome to the MyGameList website!</h1>
+        <p>Here you can add games to your list and manage them.</p>
+    </div>
+</body>
+</html>
