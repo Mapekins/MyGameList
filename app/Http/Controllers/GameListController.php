1f246fbf57bcb6125e85b731f3f49cab456d08b1
Mapekins <mapkuss@inbox.lv>
2024-06-19 11:48:45 +0300

    added new models Gamelist and Review and migrations to them

---
+++ app/Http/Controllers/GameListController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GameList;
+use Illuminate\Http\Request;
+
+class GameListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GameList $gameList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(GameList $gameList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, GameList $gameList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(GameList $gameList)
+    {
+        //
+    }
+}
